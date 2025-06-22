51a155321fc6d7de0c01be0418b5d8af41b5c16f
nguyen123 <dinh2671@gmail.com>
2025-06-22 09:57:30 +0700

    nhap tu ban phim so luong phan tu cua mang

--- chuoi/main.c
+++ chuoi/main.c
@@ -1,31 +1,18 @@
 ﻿#include <stdio.h>
-#include <string.h>
-//bài 7: Xây dựng hàm. dùng để tìm chuỗi subStr trong chuỗi str. hàm trả về địa chỉ bất đầu của subStr trong chuỗi str
-long long findSubStr(const char *str, const char *subStr) {
-	for (int i = 0; str[i] != '\0'; i++) {
-		if( str[i] == subStr[0]) {
-			int j;
-			for (j = 0; subStr[j] != '\0'; j++) {
-
-				if (str[i + j] != subStr[j]) {
-					break;
-				}
-			}
-			if (subStr[j] == '\0') { 
-				return (long long)(str + i); 
-			}
-		}
-		else if (subStr[0] == '\0') {
-			return NULL;
-		}
-	}
-}
 int main() {
-	char str[100], subStr[50];
-	printf("Nhap chuoi: ");
-	fgets(str, sizeof(str), stdin);
-	printf("Nhap chuoi con: ");
-	fgets(subStr, sizeof(subStr),stdin);
-	int result = findSubStr(str, subStr);
-	printf("Dia chi bat dau cua chuoi con trong chuoi la: %lld\n", result);
+	// nhap tu ban phim so luong phan tu cua mang
+	//sau do nhap tung phan tu cua mang
+	//in tong cua cac phan tu trong mang
+	int n, i, sum = 0;
+	int arr[100];
+	printf("Nhap so luong phan tu cua mang: ");
+	scanf_s("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf_s("%d",arr+i);
+	}
+	for (i = 0; i < n; i++) {
+		sum += arr[i];
+	}	
+	printf("Tong cua cac phan tu trong mang la: %d\n", sum);
+	return 0;
 }
